74e3d6b710e56afbb10760d45aea55792e853aa3
danniadelap <danniadelap@gmail.com>
2022-03-10 20:40:13 -0600

    Se modifica el ultimo problema

--- Actividad_3/5.js
+++ Actividad_3/5.js
@@ -2,22 +2,35 @@ var numero1 = Number(prompt("Ingrese un primer numero"));
 var numero2 = Number(prompt("Ingrese un primer numero"));
 var numero3 = Number(prompt("Ingrese un primer numero"));
 
-if (numero1 >= numero2 >= numero3) {
+if (numero1 >= numero2 && numero1>= numero3) {
     console.log("El numero mayor es", numero1);
     
-} else if (numero1 >= numero3 >= numero2) {
+} else if (numero1 >= numero3 && numero1>= numero2) {
     console.log("El numero mayor es", numero1);
     
-}else if (numero2 >= numero1 >= numero3) {
+}else if (numero2 >= numero1 && numero2>= numero3) {
     console.log("El numero mayor es", numero2);
     
-}else if (numero2 >= numero3 >= numero1) {
+}else if (numero2 >= numero3 && numero2>= numero1) {
     console.log("El numero mayor es", numero2);
     
-}else if (numero3 >= numero1 >= numero2) {
+}else if (numero3 >= numero1 && numero3>= numero2) {
     console.log("El numero mayor es", numero3);
     
-}else if (numero3 >= numero2 >= numero1) {
+}else if (numero3 >= numero2 && numero3>= numero1) {
     console.log("El numero mayor es", numero3);
+    
+}else if (numero3 == numero2 == numero1) {
+    console.log("Los numeros son iguales", numero3);
+    
+}
+else if (numero1 == numero2 && numero1 != numero3) {
+    console.log("Los numeros son iguales", numero1, numero2);
+    
+}else if (numero2 == numero3 && numero2 != numero1) {
+    console.log("Los numeros son iguales", numero2, numero3);
+    
+}else if (numero3 == numero1 && numero3 != numero1) {
+    console.log("Los numeros son iguales", numero3, numero1);
     
 }
